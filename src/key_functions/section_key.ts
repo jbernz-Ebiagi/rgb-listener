f3e61004adb93681a97e7a3eec0408194d449719
nodemon <bernelectronic@gmail.com>
2024-02-12 22:43:11 -0500

    modify sections

--- src/key_functions/section_key.ts
+++ src/key_functions/section_key.ts
@@ -4,18 +4,30 @@ import { parseXControl } from "./key_map"
 import { flash } from "../animations"
 
 const sectionKey = {
-  xControls: [
-    "select_section {param_name}"
-  ],
   commands: {
     on: (state: State, self: IKey): Command[] => {
-    //   return [['XCONTROL', parseXControl(self.xControls[0],self)]]
-      return [['XCONTROL', parseXControl(self.xControls[0], self, state.sectionPage, state.sectionPageSize)]]
+      if (state.modifiers.lshift) {
+        return [['XCONTROL', parseXControl(self.xControls[1],self, state.sectionPage, state.sectionPageSize)]] //stop_loop
+      }
+      if (state.modifiers.lalt) {
+        return [['XCONTROL', parseXControl(self.xControls[3],self, state.sectionPage, state.sectionPageSize)]]
+      }
+      if (state.modifiers.lctrl) {
+        return [['XCONTROL', parseXControl(self.xControls[4],self, state.sectionPage, state.sectionPageSize)]] 
+      }
+      return [['XCONTROL', parseXControl(self.xControls[0],self, state.sectionPage, state.sectionPageSize)]] //select_loop
     },
     off: (state: State, self: IKey): Command[] => {
-      return []
+      return [['XCONTROL', parseXControl(self.xControls[2],self, state.sectionPage, state.sectionPageSize)]]
     }
   },
+  xControls: [
+    'select_section {param_name}',
+    'stop_section {param_name}',
+    'deselect_section {param_name}',
+    "assign_snap {param_name}",
+    "clear_snap {param_name}",
+  ],
   color: (state, self) => {
     const section = state.ableton.sections[state.sectionPage*state.sectionPageSize + self.param_name]
     if (section) {
